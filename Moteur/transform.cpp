427bad705728fb18450db1d83267a5e45f1c565f
antoine <apichard@gaming.tech>
2024-02-28 16:04:21 +0100

    new rotate

--- Moteur/transform.cpp
+++ Moteur/transform.cpp
@@ -28,39 +28,44 @@ using namespace DirectX;
 
 
 		// Créer un quaternion pour chaque rotation (delta)
-
-		const XMFLOAT3 dir = fDir;
-		const XMFLOAT3 right = fRight;
-		const XMFLOAT3 up = fUp;
+		const XMFLOAT3 _dir = fDir;
+		const XMFLOAT3 _right = fRight;
+		const XMFLOAT3 _up = fUp;
 
 
 		//XMFLOAT4 quat1;
 		XMVECTOR quat;
 		XMVECTOR quatRot;
+		XMVECTOR quatCurrentRot = XMLoadFloat4(&qRot);
 
-		quat = XMQuaternionRotationAxis(XMLoadFloat3(&dir), roll);
+		quat = XMQuaternionRotationAxis(XMLoadFloat3(&_dir), roll);
 		quatRot = quat;
-		quat = XMQuaternionRotationAxis(XMLoadFloat3(&right), pitch);
+		quat = XMQuaternionRotationAxis(XMLoadFloat3(&_right), pitch);
 		quatRot *= quat;
-		quat = XMQuaternionRotationAxis(XMLoadFloat3(&up), yaw);
+		quat = XMQuaternionRotationAxis(XMLoadFloat3(&_up), yaw);
 		quatRot *= quat;
 
 		// Ajouter la rotation delta à la rotation actuelle de l’objet
-		mRot *= quatRot;
+		quatCurrentRot *= quatRot;
+
+		XMStoreFloat4(&qRot, quatCurrentRot);
 
 		// Convertir le quaternion en une matrice de rotation (magique)
 		XMMATRIX matRot;
 		matRot = XMMatrixRotationQuaternion(quatRot);
+		XMFLOAT4X4 mRot;
+		XMStoreFloat4x4(&mRot, matRot);
+
 
 		// Mettre à jour les axes de notre objet (3 vecteurs)
-		fRight.x = matRot.r[0].m128_f32[0];
-		fRight.y = matRot.r[0].m128_f32[1];
-		fRight.z = matRot.r[0].m128_f32[2];
-		fUp.x = matRot.r[1].m128_f32[0];
-		fUp.y = matRot.r[1].m128_f32[1];
-		fUp.z = matRot.r[1].m128_f32[2];
-		fDir.x = matRot.r[2].m128_f32[0];
-		fDir.y = matRot.r[2].m128_f32[1];
-		fDir.z = matRot.r[2].m128_f32[2];
+		fRight.x = mRot._11;
+		fRight.y = mRot._12;
+		fRight.z = mRot._13;
+		fUp.x = mRot._21;
+		fUp.y = mRot._22;
+		fUp.z = mRot._23;
+		fDir.x = mRot._31;
+		fDir.y = mRot._32;
+		fDir.z = mRot._33;
 
 	}
