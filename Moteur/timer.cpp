631a7c6a0ee1a3abefbffa6392d37b4aac1cb793
doriansimonet <dsimonet@gaming.tech>
2024-02-28 23:36:32 +0100

    timer

--- Moteur/timer.cpp
+++ Moteur/timer.cpp
@@ -1,1 +1,33 @@
 #include "timer.h"
+
+Timer::Timer() {
+    _fSecondsPerCount = 0.f;
+    _fDeltaTime = -1.f;
+}
+
+
+float Timer::TotalTime() const
+{
+    return 0.0f;
+}
+
+float Timer::DeltaTime() const
+{
+    return _fDeltaTime;
+}
+
+void Timer::Reset()
+{
+}
+
+void Timer::Start()
+{
+}
+
+void Timer::Stop()
+{
+}
+
+void Timer::Tick()
+{
+}
