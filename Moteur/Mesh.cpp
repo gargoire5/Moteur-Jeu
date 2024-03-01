811905a839baa294f5f2b8b445fe562a2bdcbbfc
antoine <apichard@gaming.tech>
2024-03-01 13:04:15 +0100

    init mesh

--- Moteur/Mesh.cpp
+++ Moteur/Mesh.cpp
@@ -1,1 +1,48 @@
 #include "Mesh.h"
+#include "Source/d3dUtil.h"
+#include "Graphics.h"
+
+
+void Mesh::Init()
+{
+
+}
+
+void Mesh::setComponent(ID3D12Device* meshDevice)
+{
+    /*
+    Vertex trianglesVertices[] = //1ligne = 1 point
+    {
+        { { 0.0f, 0.25f , 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+        { { 0.25f, -0.25f , 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+        { { -0.25f, -0.25f , 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+    };
+    */
+
+    VertexTriangles triangles[] = //1ligne = 1 triangle
+    {
+        { { { 0.0f, 0.25f , 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } }, { { 0.25f, -0.25f , 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },{ { -0.25f, -0.25f , 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }},
+        { { { 0.0f, 0.25f , 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } }, { { 0.25f, -0.25f , 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },{ { -0.25f, -0.25f , 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }}
+    };
+
+    const UINT vertexBufferSize = sizeof(triangles);
+    meshDevice->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+        D3D12_HEAP_FLAG_NONE,
+        &CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+        D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr,
+        IID_PPV_ARGS(&m_vertexBuffer));
+
+    // Copy the triangle data to the vertex buffer.
+    UINT8* pVertexDataBegin;
+    CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+    ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+    memcpy(pVertexDataBegin, triangles, sizeof(triangles));
+    m_vertexBuffer->Unmap(0, nullptr);
+
+    // Initialize the vertex buffer view.
+    m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+    m_vertexBufferView.StrideInBytes = sizeof(Vertex);
+    m_vertexBufferView.SizeInBytes = vertexBufferSize;
+}
--- Moteur/Mesh.h
+++ Moteur/Mesh.h
@@ -1,8 +1,31 @@
 #pragma once
 #include "Component.h"
+#include "Incl.h"
+
+using namespace DirectX;
+
 class Mesh : public Component
 {
-public:
-	
-};
+protected:
 
+	struct Vertex //1 point 
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
+
+	struct VertexTriangles // 3points = un triangle
+	{
+		Vertex v1;
+		Vertex v2;
+		Vertex v3;
+	};
+
+	// App resources.
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
+public:
+
+	void Init();
+	void setComponent(ID3D12Device* meshDevice);
+};
